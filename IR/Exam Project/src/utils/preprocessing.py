1b3c92c0c33cd1ae6553465d30e90749ef5c1c93
simdeistud <simone.deiana@studenti.units.it>
2026-04-24 11:42:17 +0200

    Added initial preprocessing functionalities

---
+++ IR/Exam Project/src/utils/preprocessing.py
@@ -0,0 +1,14 @@
+import nltk
+
+def tokenize(text: str, normalization: bool = True) -> list[str]:
+    return nltk.word_tokenize(text.lower(), language="english") if normalization else nltk.word_tokenize(text, language="english")
+
+def remove_stopwords(tokens: list[str], lang: str = "english") -> list[str]:
+    return [word for word in tokens if word.isalnum() and word not in set(nltk.corpus.stopwords.words(lang))]
+
+def stem(tokens: list[str], method: str = "porter") -> list[str]:
+    stemmer = {
+        "porter": nltk.PorterStemmer().stem,
+        "wordnet": nltk.WordNetLemmatizer().lemmatize,
+    }[method]
+    return [stemmer(word) for word in tokens]
