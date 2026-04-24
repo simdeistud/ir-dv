1bf9deaeb0885f97abf401bc842568b921b57f24
simdeistud <simone.deiana@studenti.units.it>
2026-04-24 11:21:58 +0200

    Added initial index, posting, and posting list structure

--- IR/Exam Project/src/index.py
+++ IR/Exam Project/src/index.py
@@ -1,5 +1,45 @@
+from collections import OrderedDict
+
+class Posting:
+    def __init__(self, docID):
+        self.docID = docID
+
+    def __eq__(self, other):
+        return self.docID == other.docID
+
+    def __gt__(self, other):
+        return self.docID > other.docID
+
+    def __str__(self):
+        return str(self.docID)
+
+class PostingList:
+    def __init__(self):
+        self.postings = []
+
+    def add_posting(self, posting):
+        self.postings.append(posting)
+        self.postings = sorted(self.postings, key=lambda posting: posting.docID)
+
+    def merge(self, other):
+        self.postings.extend(other)
+        self.postings = sorted(set(self.postings), key=lambda posting: posting.docID)
+
 class Index:
-    def __init__(self, corpus):
+    def __init__(self):
         self.terms = {}
 
+    def build(self, corpus):
+        for document in corpus:
+            for term in set(document.terms):
+                if term not in self.terms:
+                    self.terms[term] = PostingList()
+                self.terms[term].add_posting(Posting(document.docID))
+        self.terms = dict(sorted(self.terms.items()))
+
+    def load(self, path):
+
+    def save(self, path):
+
     def merge(self, corpus):
+
