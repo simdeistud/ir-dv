cc4afd7f70331470984c1c37c0941845a801eeaf
simdeistud <simone.deiana@studenti.units.it>
2026-04-26 18:05:32 +0200

    Implemented wildcard search

--- IR/Exam Project/src/IR.py
+++ IR/Exam Project/src/IR.py
@@ -5,7 +5,7 @@ from .index import *
 
 class IR:
     def __init__(self, path: str):
-        self.index = Index()
+        self.index = InvertedPermutermIndex()
         self.index.build(Corpus("NTIS", path))
 
     def prepare_query(self, querystr: str):
@@ -13,8 +13,17 @@ class IR:
 
     def retrieve(self, query):
         if isinstance(query, Atom):
-            # SYSTEM QUERY
-            return self._term(query.value)
+            # SINGLE TERM QUERY
+            if isinstance(query.value, str) and "*" not in query.value:
+                return self._term(query.value)
+            # WILDCARD SINGLE TERM QUERY
+            elif isinstance(query.value, str) and "*" in query.value:
+                return ...
+            # PHRASE QUERY
+            elif isinstance(query.value, list):
+                ...
+            else:
+                raise TypeError
 
         if isinstance(query, Not):
             child_ir = self.retrieve(query.node)
@@ -30,13 +39,32 @@ class IR:
             right_ir = self.retrieve(query.right)
             return self._or(left_ir, right_ir)
 
-        raise TypeError(f"Unknown node {query}")
+        raise TypeError(f"Unknown value {query}")
 
     def _term(self, term: str) -> set[Posting]:
-        return self.index.terms.get(term, None)
+        return set(self.index[f"{term}$"].posting_list())
+
+    def _wildcard_term(self, term: str) -> set[Posting]:
+        if term.count("*") > 1:
+            raise NotImplementedError("More than 1 wildcard is not supported for permuterm indexes")
+        # We create rotate the term until the wildcard is at the end
+        term = self._prefix_wildcard(f"{term}$")
+        # We obtain the posting lists of all the matches
+        posting_lists = [t.posting_list() for t in self.index.get_matching(term)]
+        # We return the union of their postings
+        result = PostingList()
+        for posting_list in posting_lists:
+            result.merge(posting_list)
+        return set(result)
+
+    def _prefix_wildcard(self, term: str) -> str:
+        rotation = term
+        while rotation[-1] != "*":
+            rotation = rotation[1:] + rotation[0]
+        return rotation
 
     def _not(self, p: set[Posting]) -> set[Posting]:
-        return self.index.docIDs - p
+        return self.index._postings_idx - p
 
     def _and(self, lp: set[Posting], rp: set[Posting]) -> set[Posting]:
         return lp & rp
