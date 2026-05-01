e9c6d8fbb94157a00e8da25cc82f8523a03c7cb9
simdeistud <simone.deiana@studenti.units.it>
2026-05-01 16:50:19 +0200

    Refactoring to remove common code

--- IR/Exam Project/src/index.py
+++ IR/Exam Project/src/index.py
@@ -2,7 +2,7 @@ import json
 from dataclasses import dataclass
 from functools import total_ordering
 from .utils import preprocessing
-from .utils.corpus import Corpus
+from .utils.corpus import *
 
 class PostingList:
     def __init__(self, postings: dict[str, list[int]] = None):
@@ -34,15 +34,15 @@ class PostingList:
 
 class InvertedIndex:
     def __init__(self):
-        # The main index is an ordered list of Terms
+        # The main index is a dictionary of posting lists
         self._index: dict[str, PostingList] = {}
         # We keep a set of all the docIDs to make answering NOT queries simpler
-        self._postings_idx: set[str] = set()
+        self._postings_index: set[str] = set()
 
     def build(self, corpus: Corpus) -> None:
         for document in corpus:
             print(f"adding document to index [{document.docID}]")
-            tokens = preprocessing.tokenize(document.title + " " + document.text)
+            tokens = preprocessing.tokenize(document.title + " " + document.main_text)
             # First we add all the base terms to the main index
             i = 0 # Keep track of token position to fill positional index of every posting
             for t in tokens:
@@ -50,7 +50,7 @@ class InvertedIndex:
                 if t in self._index:
                     self._index[t].merge(current)
                 else:
-                    self._postings_idx.add(document.docID)  # We add the posting to the index
+                    self._postings_index.add(document.docID)  # We add the posting to the index
                     self._index[t] = current
                 i += 1
 
@@ -60,28 +60,15 @@ class InvertedIndex:
     def __len__(self):
         return len(self._index)
 
-class InvertedPermutermIndex:
+class InvertedPermutermIndex(InvertedIndex):
     def __init__(self):
-        # The main index is an ordered list of Terms
-        self._index: dict[str, PostingList] = {}
+        super().__init__()
+        # Create auxiliary index of permuterms
         self._permuterm_index: dict[str, str] = {}
-        # We keep a set of all the docIDs to make answering NOT queries simpler
-        self._postings_idx: set[str] = set()
 
-    def build(self, corpus: Corpus) -> None:
-        for document in corpus:
-            print(f"adding document to index [{document.docID}]")
-            tokens = preprocessing.tokenize(document.title + " " + document.text)
-            # First we add all the base terms to the main index
-            i = 0  # Keep track of token position to fill positional index of every posting
-            for t in tokens:
-                current = PostingList({document.docID: [i]})
-                if t in self._index:
-                    self._index[t].merge(current)
-                else:
-                    self._postings_idx.add(document.docID)  # We add the posting to the index
-                    self._index[t] = current
-                i += 1
+
+    def build(self, corpus: CranfieldCorpus) -> None:
+        super().build(corpus)
         for term in self._index:
             permuterms = self._get_permuterms(term)
             for permuterm in permuterms:
@@ -102,32 +89,15 @@ class InvertedPermutermIndex:
             return [self._index[term] for term in terms]
         else: return self._index[item] if item in self._index else PostingList()
 
-    def __len__(self):
-        return len(self._index)
-
-class InvertedKGramIndex:
+class InvertedKGramIndex(InvertedIndex):
     def __init__(self, k: int):
-        # The main index is an ordered list of Terms
-        self._index: dict[str, PostingList] = {}
-        self._kgram_index: dict[str, set[str]] = {}
+        super().__init__()
+        # Create auxiliary index of k-grams
         self._k = k
-        # We keep a set of all the docIDs to make answering NOT queries simpler
-        self._postings_idx: set[str] = set()
-
-    def build(self, corpus: Corpus) -> None:
-        for document in corpus:
-            print(f"adding document to index [{document.docID}]")
-            tokens = preprocessing.tokenize(document.title + " " + document.text)
-            # First we add all the base terms to the main index
-            i = 0  # Keep track of token position to fill positional index of every posting
-            for t in tokens:
-                current = PostingList({document.docID: [i]})
-                if t in self._index:
-                    self._index[t].merge(current)
-                else:
-                    self._postings_idx.add(document.docID)  # We add the posting to the index
-                    self._index[t] = current
-                i += 1
+        self._kgram_index: dict[str, set[str]] = {}
+        
+    def build(self, corpus: CranfieldCorpus) -> None:
+        super().build(corpus)
         for term in self._index:
             kgrams = self._get_kgrams(f"${term}$")
             for kgram in kgrams:
@@ -179,6 +149,3 @@ class InvertedKGramIndex:
                 for term in common_termset:
                     result.merge(self.get_from_term(term))
         return result
-
-    def __len__(self):
-        return len(self._index)
