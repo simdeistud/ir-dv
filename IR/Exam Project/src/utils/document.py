10a72ca1abf314c52054df516f124fa3f5579138
simdeistud <simone.deiana@studenti.units.it>
2026-05-01 16:40:56 +0200

    Added intermediary class to represent indexed documents with an ordering

--- IR/Exam Project/src/utils/document.py
+++ IR/Exam Project/src/utils/document.py
@@ -3,17 +3,33 @@ class Document:
         self.raw_text = raw_text
     def __str__(self):
         return self.raw_text
-    
-class CranfieldDocument(Document):
-    def __init__(self, raw_text: str):
+
+
+class IndexedDocument(Document):
+    def __init__(self, raw_text: str, docID: int):
         super().__init__(raw_text)
-        fields = self._parse(self.raw_text)
-        self.docID = fields["I"]
+        self.docID = docID
+    
+    def __str__(self):
+        return f"docID : {self.docID}\n" + f"raw_text : {self.raw_text}\n"
+    
+    def __hash__(self) -> int:
+        return hash(self.docID)
+    
+    def __eq__(self, other: IndexedDocument):
+        return self.docID == other.docID
+    
+    def __lt__(self, other: IndexedDocument):
+        return self.docID > other.docID
+    
+class CranfieldDocument(IndexedDocument):
+    def __init__(self, raw_text: str):
+        fields = self._parse(raw_text)
+        super().__init__(raw_text, int(fields["I"]))
         self.title = fields["T"]
         self.authors = fields["A"]
         self.metadata = fields["B"]
         self.main_text = fields["W"]
-        print(self)    
 
     @staticmethod
     def _parse(raw_text: str) -> dict[str, str]:
